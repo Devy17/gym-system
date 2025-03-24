c0a5cedf8274db05ec9711128c75b00928bc4acd
hyeonguk <shyunguk126@naver.com>
2025-03-24 22:03:14 +0900

    회원 등록 에러 수정

--- src/jdbc/DBConnectionManager.java
+++ src/jdbc/DBConnectionManager.java
@@ -8,8 +8,8 @@ public class DBConnectionManager {
     // 오라클 JDBC 연결 정보
     private static final String DRIVER = "oracle.jdbc.driver.OracleDriver"; // 정해져있는 값
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe"; //데이터베이스에 요청을 보내는 주소 -> 접속
-    private static final String USER = ""; // TODO 작성해주세요
-    private static final String PASSWORD = ""; // TODO 작성해주세요
+    private static final String USER = "shin"; // TODO 작성해주세요
+    private static final String PASSWORD = "shin"; // TODO 작성해주세요
 
     // 정적 초기화자(static initializer)를 사용하여 드라이버 로드
     static {
