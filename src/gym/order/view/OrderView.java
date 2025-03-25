cd3ce44f9446bee615561335978739035d19f4b0
HyunHee <h2koo2004@gmail.com>
2025-03-25 12:41:24 +0900

    test

--- src/gym/order/view/OrderView.java
+++ src/gym/order/view/OrderView.java
@@ -3,14 +3,27 @@ package gym.order.view;
 import static common.AppUI.inputInteger;
 import static common.AppUI.wrongNumber;
 
+import gym.membership.domain.Membership;
+import gym.membership.service.MembershipService;
+import java.text.DecimalFormat;
+import java.util.List;
+
 public class OrderView {
+
     public static void purchaseMembershipView() {
-        // TODO 여기에 회원권 조회하는 로직이 들어가야 함. -> membership.service에 구현해주세요
-//        System.out.println("\n# 구매하시려는 회원권을 선택해주세요.");
-//        System.out.println("### 1. 3개월");
-//        System.out.println("### 2. 6개월");
-//        System.out.println("### 3. 9개월");
-//        System.out.println("### 4. 12개월");
+        List<Membership> membershipOptions = MembershipService.getMembershipOptions();
+        System.out.println("\n# 구매하시려는 회원권을 선택해주세요.");
+
+        for (int i = 1; i <= membershipOptions.size(); i++) {
+            Membership membership = membershipOptions.get(i - 1);
+
+            DecimalFormat formatter = new DecimalFormat("#,###");
+            String formattedPrice = formatter.format(membership.getPrice());
+
+            System.out.println("### " + i + ". "
+                    + membership.getPeriod() + "개월 - " + formattedPrice + "원");
+        }
+
 
         int selectNum = inputInteger(">>> ");
         switch (selectNum) {
--- src/gym/product/repo/ProductRepository.java
+++ src/gym/product/repo/ProductRepository.java
@@ -1,4 +1,35 @@
 package gym.product.repo;
 
+import gym.membership.domain.Membership;
+import gym.product.domain.Product;
+import jdbc.DBConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepository {
+    public List<Product> findAll() {
+        List<Product> productList = new ArrayList<>();
+        String sql = "SELECT " +
+
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                productList.add(new Membership(
+                        rs.getInt("price"),
+                        rs.getInt("period"),
+                        rs.getInt("membership_id")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return membershipList;
+    }
 }
