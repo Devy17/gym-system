3339beb067c3c63948563814cda911d87bdc85dc
hyeonguk <shyunguk126@naver.com>
2025-03-27 12:13:10 +0900

    회원기간 종료된 회원이 다시 재결제 할 수 있도록 수정

--- src/gym/user/repo/UserRepository.java
+++ src/gym/user/repo/UserRepository.java
@@ -106,11 +106,15 @@ public class UserRepository {
 
     public List<User> findByUserName(String name, boolean flag) {
         List<User> userList = new ArrayList<>();
-        String sql = "SELECT * FROM users WHERE user_name = ? AND user_active ?";
+        String sql = flag ? "SELECT * FROM users WHERE user_name = ? AND user_active = ?" :
+                "SELECT * FROM users WHERE user_name = ? AND user_active IN(?, ?)";
         try (Connection conn = DBConnectionManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, name);
-            pstmt.setString(2, flag ? "= Y" : "IN('Y', 'N')");
+            pstmt.setString(2, "Y");
+            if(!flag) {
+                pstmt.setString(3, "N");
+            }
 
             ResultSet rs = pstmt.executeQuery();
             while(rs.next()) {
